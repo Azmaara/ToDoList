b9bb6c72ac3bd836683521926a787e55c9e2d8f9
Farhan <farhannahmad.fna@gmail.com>
2020-04-11 23:17:18 +0300

    Update Script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li");
 
 function inputLength() {
 	return input.value.length;
@@ -11,7 +12,30 @@ function createListElement() {
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+	
+	var button = document.createElement("button");
+	button.appendChild(document.createTextNode("Done!"));
+	li.appendChild(button);
+	button.onclick = underlineParent;
+
+// CREATE AN ELEMENT
+	var button = document.createElement("button"); 
+// APPEND TEXT TO BUTTON
+	button.appendChild(document.createTextNode("Delete!"));
+// APPEND BUTTON TO LIST
+	li.appendChild(button);
+	
+	
 }
+
+function underlineParent(event){
+	event.target.parentNode.classList.toggle("done");
+}
+
+
+function removeParent(evt){
+	evt.target.parentNode.remove();
+} 
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
